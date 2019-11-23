73a85f5cc9f911609ffb281e1865cfe3beb79460
sotirisloizidis <55096913+sotirisloizidis@users.noreply.github.com>
2019-11-24 01:54:09 +0200

    Add files via upload
    scripts for statistcs.html chart

---
+++ js/chart.js
@@ -0,0 +1,29 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+type: 'bar',
+data: {
+labels: ["Physically Signed-In", "Enrolled"],
+datasets: [{
+label: '# of People',
+data: [12, 19],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)'
+],
+borderColor: [
+'rgba(255,99,132,1)',
+'rgba(54, 162, 235, 1)'
+],
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero: true
+}
+}]
+}
+}
+});
