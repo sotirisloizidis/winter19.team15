3d5f0ffd54e110d3ccb838981479d187a681bb46
asavva01 <38720882+asavva01@users.noreply.github.com>
2019-11-24 01:07:10 +0200

    Add files via upload

---
+++ js/enroll.js
@@ -0,0 +1,34 @@
+function changeBt1() 
+{
+    var elem = document.getElementById("enrollBtn");
+    if (elem.value=="Enroll") elem.value = "UnEnroll";
+    else elem.value = "Enroll";
+}
+
+function changeBt2() 
+{
+    var elem = document.getElementById("enrollBtn2");
+    if (elem.value=="Enroll") elem.value = "UnEnroll";
+    else elem.value = "Enroll";
+}
+
+function changeBt3() 
+{
+    var elem = document.getElementById("enrollBtn3");
+    if (elem.value=="Enroll") elem.value = "UnEnroll";
+    else elem.value = "Enroll";
+}
+
+function changeBt4() 
+{
+    var elem = document.getElementById("enrollBtn4");
+    if (elem.value=="Enroll") elem.value = "UnEnroll";
+    else elem.value = "Enroll";
+}
+
+function changeBt5() 
+{
+    var elem = document.getElementById("enrollBtn5");
+    if (elem.value=="Enroll") elem.value = "UnEnroll";
+    else elem.value = "Enroll";
+}
