2625810cfa18faeb896f62c5eff2bfa058fd875e
sotirisloizidis <sloizi02@cs.ucy.ac.cy>
2020-02-13 23:50:33 +0200

    aDDED ALL PHP SCRIPTS TO DATE

---
+++ php/Form.php
@@ -0,0 +1,17 @@
+  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  <title></title>
+  </head>
+  <body>
+         <form method="POST" action="submit.php">
+         Name:<input type="text" name="Name" id="name" >
+         Address:<input type="text" name="Address" id="address">
+         Telephone:<input type="text" name="Telephone" id="tele">
+         Email:<input type="text" name="Email" id="email">
+         Password:<input type="text" name="Password" id="pass">
+         <input type="submit" value="Submit">
+  </body>
+</html>
