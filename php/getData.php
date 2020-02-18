ebbd7f6b5035015fa14934cd764b38d04ed86b90
mkaili02 <mkaili02@cs.ucy.ac.cy>
2020-02-18 22:08:00 +0200

    added include connectDB to getData.php

--- php/getData.php
+++ php/getData.php
@@ -1,15 +1,7 @@
 <?php
 
-    $servername="localhost";
-    $username = "ironsky";
-    $password = "pfVGdTzSOoLh85yp";
-    $dbname = "ironsky";
+   include 'connectDB.php';
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
      if(isset($_POST['days'])){
         $day= $_POST['days'];
     }
